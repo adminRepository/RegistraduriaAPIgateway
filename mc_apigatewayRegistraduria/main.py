21d0892ce510067e8122a3bd0281de68394ff38b
unknown <yesnarvaez15@gmail.com>
2023-11-22 13:46:22 -0500

    implementacion del metodo login en gateway

--- mc_apigatewayRegistraduria/main.py
+++ mc_apigatewayRegistraduria/main.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import json
 from waitress import serve
 import datetime
-import requests
+import request
 import re
 
 app = Flask(__name__)
@@ -34,3 +34,26 @@ if __name__ == '__main__':
     serve(app, host=dataConfig["url-backend"], port=dataConfig["port"])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+from flask_jwt_extended import create_access_token, verify_jwt_in_request
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+
+app.config["JWT_SECRET_KEY"] = "super-secret" # Cambiar por el que seconveniente
+jwt = JWTManager(app)
+@app.route("/login", methods=["POST"])
+def create_token():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url=dataConfig["url-backend-security"]+'/usuarios/validar'
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        user = response.json()
+        expires = datetime.timedelta(seconds=60 * 60*24)
+        access_token = create_access_token(identity=user,
+expires_delta=expires)
+        return jsonify({"token": access_token, "user_id": user["_id"]})
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
