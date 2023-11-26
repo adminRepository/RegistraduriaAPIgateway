1fcff329bd5f4b2a127b0befd7d952b946d9362c
Ebenavides <ebenavidess095@gmail.com>
2023-11-26 11:48:48 -0500

    Implementacion de Middleware en Gateway

--- mc_apigatewayRegistraduria/main.py
+++ mc_apigatewayRegistraduria/main.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import json
 from waitress import serve
 import datetime
-import request
+import requests
 import re
 
 app = Flask(__name__)
@@ -52,8 +52,85 @@ def create_token():
     if response.status_code == 200:
         user = response.json()
         expires = datetime.timedelta(seconds=60 * 60*24)
-        access_token = create_access_token(identity=user,
-expires_delta=expires)
+        access_token = create_access_token(identity=user,expires_delta=expires)
         return jsonify({"token": access_token, "user_id": user["_id"]})
     else:
         return jsonify({"msg": "Bad username or password"}), 401
+
+
+
+@app.before_request
+def before_request_callback():
+    endPoint = limpiarURL(request.path)
+    excludedRoutes = ["/login"]
+    if excludedRoutes.__contains__(request.path):
+        pass
+    elif verify_jwt_in_request():
+        usuario = get_jwt_identity()
+        if usuario["rol"] is not None:
+            tienePersmiso = validarPermiso(endPoint, request.method,usuario["rol"]["_id"])
+            if not tienePersmiso:
+               return jsonify({"message": "Permission denied"}), 401
+        else:
+            return jsonify({"message": "Permission denied"}), 401
+def limpiarURL(url):
+    partes = url.split("/")
+    for laParte in partes:
+        if re.search('\\d', laParte):
+            url = url.replace(laParte, "?")
+    return url
+def validarPermiso(endPoint, metodo, idRol):
+    url = dataConfig["url-backend-security"] + "/permisos-roles/validarpermiso/rol/" + str(idRol)
+    tienePermiso = False
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    body = {
+        "url": endPoint,
+        "metodo": metodo
+    }
+    response = request.get(url, json=body, headers=headers)
+    try:
+        data = response.json()
+        if ("_id" in data):
+            tienePermiso = True
+    except:
+        pass
+    return tienePermiso
+
+@app.route("/estudiantes", methods=['GET'])
+def getEstudiantes():
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/estudiantes'
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+@app.route("/estudiantes", methods=['POST'])
+def crearEstudiante():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/estudiantes'
+    response = requests.post(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+@app.route("/estudiantes/<string:id>", methods=['GET'])
+def getEstudiante(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/estudiantes/' + id
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+@app.route("/estudiantes/<string:id>", methods=['PUT'])
+def modificarEstudiante(id):
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/estudiantes/' + id
+    response = requests.put(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+@app.route("/estudiantes/<string:id>", methods=['DELETE'])
+def eliminarEstudiante(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/estudiantes/' + id
+    response = requests.delete(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
